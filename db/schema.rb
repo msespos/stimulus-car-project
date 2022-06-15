8f5de42db76211db0530f2be40f453a222c53e74
Mike Esposito <mikeesposito6852@gmail.com>
2022-06-14 18:42:04 -0700

    Add car_id to variants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_015416) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_013133) do
   create_table "cars", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_015416) do
     t.boolean "tape"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "car_id"
   end
 
 end
