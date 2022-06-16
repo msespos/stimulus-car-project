e7f7f14271452ea0dc503eee3102370c1bf1c2d5
Mike Esposito <mikeesposito6852@gmail.com>
2022-06-16 12:08:36 -0700

    Add new create and car_params to Cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,22 @@
 class CarsController < ApplicationController
+  def new
+    @car = Car.new
+    3.times { @car.variants.build }
+  end
+
+  def create
+    @car = Car.new(car_params)
+    Rails.logger.debug "CAR PARAMS = #{car_params}"
+    if @car.save
+      redirect_to @car
+    else
+      render "new"
+    end
+  end
+
+  private
+
+    def car_params
+      params.require(:car).permit(:name, variants_attributes: [:color, :seats, :tape])
+    end
 end
