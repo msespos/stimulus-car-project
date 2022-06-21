363ed07ead332d56b92065a76d031ded3585864b
Mike Esposito <mikeesposito6852@gmail.com>
2022-06-21 16:02:30 -0700

    Remove unnecessary code from stimulus-components.com

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,12 +6,11 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+// Below two lines from stimulus-components.com
+import { NestedForm } from 'stimulus-rails-nested-form'
+application.register('nested-form', NestedForm)
+
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
 
-import { Application } from '@hotwired/stimulus'
-import NestedForm from 'stimulus-rails-nested-form'
-
-const application = Application.start()
-application.register('nested-form', NestedForm)
