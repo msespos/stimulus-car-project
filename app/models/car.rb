f173ef95f72c3d273aed8cea8a287f2e88dba871
Mike Esposito <mikeesposito6852@gmail.com>
2022-06-21 10:26:22 -0700

    Add arguments to accepts_nested_attributes_for

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,4 @@
 class Car < ApplicationRecord
   has_many :variants
-  accepts_nested_attributes_for :variants
+  accepts_nested_attributes_for :variants, reject_if: :all_blank, allow_destroy: true
 end
